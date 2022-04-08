b82258c598079d8435db80b195e4f2c8fc4c09ab
paulsump <sumpner@hotmail.com>
2022-04-08 06:42:17 +0100

    basic ui for share buttons

--- lib/buttons/share_buttons.dart
+++ lib/buttons/share_buttons.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Add a share button for social media platforms to share the next launch with friends
 class ShareButtons extends StatelessWidget {
@@ -6,6 +8,54 @@ class ShareButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildSocialButton(
+              icon: FontAwesomeIcons.facebookSquare,
+              color: const Color(0xFF0075FC),
+              onClicked: () => share(SocialMedia.facebook),
+            ),
+            buildSocialButton(
+              icon: FontAwesomeIcons.twitter,
+              color: const Color(0xFF1da1f2),
+              onClicked: () => share(SocialMedia.twitter),
+            ),
+            buildSocialButton(
+              icon: FontAwesomeIcons.linkedin,
+              color: const Color(0xFF0072b1),
+              onClicked: () => share(SocialMedia.linkedIn),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+Future share(SocialMedia platform) async {
+  final urls = {
+    SocialMedia.facebook: ('face book shareable link'),
+    SocialMedia.twitter: ('twitter shareable link'),
+    SocialMedia.linkedIn: ('linkedIn link')
+  };
+  final url = urls[platform]!;
+  await launch(url);
+}
+
+enum SocialMedia { facebook, twitter, linkedIn }
+
+Widget buildSocialButton(
+        {required IconData icon,
+        Color? color,
+        required Function() onClicked}) =>
+    InkWell(
+      child: SizedBox(
+        width: 60,
+        height: 60,
+        child: Center(child: Icon(icon, color: color, size: 40)),
+      ),
+      onTap: onClicked,
+    );
