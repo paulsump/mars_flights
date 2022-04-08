67323d03af64145b7a5842a260c8f43100f96e46
paulsump <sumpner@hotmail.com>
2022-04-08 07:15:01 +0100

    extracted hexagonButton

--- lib/buttons/flat_hexagon_button.dart
+++ lib/buttons/flat_hexagon_button.dart
@@ -1,9 +1,41 @@
 // © 2022, Paul Sumpner <sumpner@hotmail.com>
 
+import 'package:flutter/material.dart';
 import 'package:mars_flights/buttons/hexagon_border.dart';
 import 'package:mars_flights/hue.dart';
 import 'package:mars_flights/screen_adjust.dart';
-import 'package:flutter/material.dart';
+
+/// A button with an icon and a hexagon border.
+class HexagonButton extends StatelessWidget {
+  const HexagonButton({
+    Key? key,
+    required this.onPressed,
+    required this.icon,
+    required this.tip,
+    this.color,
+  }) : super(key: key);
+
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  final String tip;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      //TODO FIx in landscape too small
+      heightFactor: 1 + screenAdjustY(0.0003, context),
+      child: IconFlatHexagonButton(
+        onPressed: onPressed,
+        tip: tip,
+        icon: icon,
+        iconSize: screenAdjustNormalIconSize(context),
+        color: color,
+      ),
+    );
+  }
+}
 
 /// Transparent flat hexagon shaped [TextButton] (without text).
 class FlatHexagonButton extends StatelessWidget {
@@ -43,18 +75,22 @@ class FlatHexagonButton extends StatelessWidget {
 
 /// A [FlatHexagonButton] with an icon.
 class IconFlatHexagonButton extends StatelessWidget {
-  final VoidCallback? onPressed;
-  final String tip;
-  final IconData icon;
-  final double iconSize;
-
   const IconFlatHexagonButton({
     Key? key,
     this.onPressed,
     required this.tip,
     required this.icon,
     required this.iconSize,
+    this.color,
   }) : super(key: key);
+
+  final VoidCallback? onPressed;
+  final String tip;
+
+  final IconData icon;
+  final double iconSize;
+
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
@@ -63,7 +99,8 @@ class IconFlatHexagonButton extends StatelessWidget {
       tip: tip,
       child: Icon(
         icon,
-        color: onPressed != null ? Hue.enabledIcon : Hue.disabledIcon,
+        color:
+            color ?? (onPressed != null ? Hue.enabledIcon : Hue.disabledIcon),
         size: iconSize,
       ),
     );
--- lib/buttons/share_buttons.dart
+++ lib/buttons/share_buttons.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mars_flights/buttons/flat_hexagon_button.dart';
-import 'package:mars_flights/screen_adjust.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// Add a share button for social media platforms to share the next launch with friends
@@ -15,54 +14,32 @@ class ShareButtons extends StatelessWidget {
     return Center(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          _ShareButton(
-            url: 'TODO facebook URL + flight text',
+        children: [
+          HexagonButton(
+            onPressed: () => unawaited(launch(
+              'TODO facebook URL + flight text',
+            )),
             icon: FontAwesomeIcons.facebookSquare,
-            // color: Color(0xFF0075FC),
+            color: const Color(0xFF0075FC),
             tip: 'Share this flight on Facebook',
           ),
-          _ShareButton(
-            url: 'TODO twitter URL + flight text',
+          HexagonButton(
+            onPressed: () => unawaited(launch(
+              'TODO twitter URL + flight text',
+            )),
             icon: FontAwesomeIcons.twitter,
-            // color: Color(0xFF1da1f2),
+            color: const Color(0xFF1da1f2),
             tip: 'Share this flight on Twitter',
           ),
-          _ShareButton(
-            url: 'TODO linkedIn URL + flight text',
+          HexagonButton(
+            onPressed: () => unawaited(launch(
+              'TODO linkedIn URL + flight text',
+            )),
             icon: FontAwesomeIcons.linkedin,
-            // color: Color(0xFF0072b1),
+            color: const Color(0xFF0072b1),
             tip: 'Share this flight on LinkedIn',
           )
         ],
-      ),
-    );
-  }
-}
-
-/// A button with an icon on it, that, when pressed
-/// uses [Navigator] to replace the current page with the chosen one.
-/// For [PageButtons].
-class _ShareButton extends StatelessWidget {
-  const _ShareButton({
-    Key? key,
-    required this.url,
-    required this.icon,
-    required this.tip,
-  }) : super(key: key);
-
-  final IconData icon;
-  final String url, tip;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      heightFactor: 1 + screenAdjustY(0.0003, context),
-      child: IconFlatHexagonButton(
-        onPressed: () => unawaited(launch(url)),
-        tip: tip,
-        icon: icon,
-        iconSize: screenAdjustNormalIconSize(context),
       ),
     );
   }
