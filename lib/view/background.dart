05c8e0e1318274c1d71bb56d63b844c63cb42897
paulsump <sumpner@hotmail.com>
2022-04-09 18:25:15 +0100

    Moved _Star to where it's needed and made private.

--- lib/view/background.dart
+++ lib/view/background.dart
@@ -6,10 +6,9 @@ import 'package:mars_flights/hue.dart';
 import 'package:mars_flights/screen_adjust.dart';
 import 'package:mars_flights/view/pulsate.dart';
 import 'package:mars_flights/view/screen_adjusted_text.dart';
-import 'package:mars_flights/view/star.dart';
 
 /// A container frame / scaffold for all pages.
-/// It has the background color and image and a silly little [Star] animation.
+/// It has the background color and image and a silly little [_Star] animation.
 class Background extends StatelessWidget {
   const Background({
     Key? key,
@@ -33,7 +32,7 @@ class Background extends StatelessWidget {
                 child: Image(image: AssetImage('images/background.jpg')),
               ),
             ),
-            const Star(),
+            const _Star(),
             ScreenAdjust(
               portrait: const Offset(0.25, 1.5),
               landscape: const Offset(0.3, 1.6),
@@ -112,6 +111,53 @@ class _PageButtons extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+/// Little animated stars on the [Background]
+class _Star extends StatefulWidget {
+  const _Star({Key? key}) : super(key: key);
+
+  @override
+  _StarState createState() => _StarState();
+}
+
+class _StarState extends State<_Star> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 80000), vsync: this)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.repeat();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = getScreenSize(context);
+
+    const unit = Offset(1, 1);
+    final center = unit * calcUnitPingPong(_controller.value);
+
+    final offset = Offset(size.width * center.dx, size.height * center.dy);
+    final imageSize = unit * 128;
+
+    return Transform.translate(
+      offset: offset - imageSize / 2,
+      child: const Pulsate(
+          scale: 0.4, child: Image(image: AssetImage('images/star.png'))),
     );
   }
 }
