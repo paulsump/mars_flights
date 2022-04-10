1eee68c4e325eb9a7cb709622be9dff140260022
paulsump <sumpner@hotmail.com>
2022-04-10 17:30:24 +0100

    fixed 'good response' widget test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,22 +6,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
 import 'package:mars_flights/main.dart';
+import 'package:mars_flights/out.dart';
 import 'package:mars_flights/view/countdown_page.dart';
 
 Future<http.Response> _getGoodResponse(http.Request url) async {
   String fixture(String name) => File('test_data/$name').readAsStringSync();
 
-  const base = 'GET https://api.spacexdata.com/v4/launches/';
+  const base = 'GET https://api.spacexdata.com/v4/';
 
   final headers = {
     HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
   };
 
+  out('p3 ${url.toString()}');
+
   switch (url.toString()) {
-    case base + 'next':
+    case base + 'launches/next':
       return http.Response(fixture('flight.json'), 200, headers: headers);
-    case base + 'upcoming':
+    case base + 'launches/upcoming':
       return http.Response(fixture('flights.json'), 200, headers: headers);
+    case base + 'launchpads':
+      out('p4');
+      return http.Response(fixture('launch_pads.json'), 200, headers: headers);
   }
   throw 'huh? ($url)';
 }
@@ -33,6 +39,8 @@ Future<http.Response> _getEmptyResponse(http.Request url) async {
     case base + 'next':
       return http.Response('{}', 200);
     case base + 'upcoming':
+      return http.Response('[]', 200);
+    case base + 'launchpads':
       return http.Response('[]', 200);
   }
   throw 'empty huh? ($url)';
